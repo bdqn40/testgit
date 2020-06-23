8a69f29bd0bac939de1028f13c7b777ad6312f9e
bhj1139734325 <1139734325@qq.com>
2020-06-23 15:18:07 +0800

    test

---
+++ src/cn/bdqn/test.java
@@ -0,0 +1,11 @@
+package cn.bdqn;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+    }
+}
